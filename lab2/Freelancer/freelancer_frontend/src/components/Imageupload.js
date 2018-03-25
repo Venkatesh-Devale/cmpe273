a11f810ad47554863d7b5d092fc50227409b20d7
Venkatesh-Devale <venkateshdevale@hotmail.com>
2018-03-25 15:08:00 -0700

    Adding lab1 to lab2 and progressing from there further
    Here I have added lab2 folder and in that added the lab1 freelancer work. Will make the changes to lab2 now for lab2 assignment.

---
+++ lab2/Freelancer/freelancer_frontend/src/components/Imageupload.js
@@ -0,0 +1,126 @@
+import React, {Component} from 'react';
+import '../css/style.css';
+import { connect } from 'react-redux';
+import axios from 'axios';
+//import image from '../images/freelancerlogo.png';
+
+class Imageuploader extends Component {
+    constructor() {
+        super();
+        this.state = {
+            fileSelected: '',
+            imagePreview: '',
+            display: 'none',
+            filename: 'default.png'
+        }
+    }
+
+    componentWillMount(){
+        
+        this.loadUserImage();
+    }
+
+    loadUserImage(){
+        
+        var self = this;
+        axios.get("http://localhost:3001/getuserimage?username=" + localStorage.getItem('username'), {withCredentials: true} )
+        .then( (response) => {
+            self.setState({
+                filename: response.data.image_name.image_name
+            })
+        })
+    }
+
+    handleChange = e => {
+        e.preventDefault();
+        console.log("bhdib", e.target.files[0].name);
+        var rdr = new FileReader();
+        var fileSelected = e.target.files[0];
+        rdr.onloadend = () => {
+          this.setState({
+            // fileSelected: fileSelected,
+            // imagePreview: rdr.result,
+            display: 'block',
+            fileSelected: fileSelected
+          });
+        }
+    
+        rdr.readAsDataURL(fileSelected);
+    }
+
+    handleUplaod = (e) => {
+        e.preventDefault();
+        const form_data = new FormData();
+        form_data.append('filevalue', this.state.fileSelected);
+        form_data.append('username', localStorage.getItem('username'));
+        const config = {
+            headers: {
+            'content-type': 'multipart/form-data'
+            }
+        }
+        if(this.state.fileSelected != ""){
+            var self =this;
+            axios.post('http://localhost:3001/saveimage', form_data, { withCredentials: true}, config)
+            .then( (response) => {
+                debugger
+                self.setState({
+                filename: "" + localStorage.getItem('username') + "." + response.data.fileType
+                    
+                })
+            })
+        }
+        else{
+            alert("Please upload an image first");
+        }
+        // console.log("In handle upload...")
+        // console.log("In mapDispatch",  this.state.fileSelected + "......" + this.state.imagePreview);
+        // const imageDetails = {
+        //     fileSelected: this.state.fileSelected,
+        //     imagePreview: this.state.imagePreview
+        // }
+        // axios.post('http://localhost:3001/saveimage', imageDetails, { withCredentials: true})
+        // .then( (response) => {
+            
+        // })
+    }
+
+
+    render() {
+    
+        // let {imagePreview} = this.state;
+        let $imagePreviewFinal = null;
+        if (this.state.filename) {
+            
+            $imagePreviewFinal = (<img src = { require('/Users/venkateshdevale/Desktop/private git/cmpe273/lab1/Freelancer/freelancer-server/images/' + this.state.filename) } alt = "This is user's display pic"/>);
+          }
+        const style ={
+            display : this.state.display
+        }
+        return(
+            <div className="Imageuploader">
+                <div id='profileImage'>
+                    {$imagePreviewFinal}
+                        <div id='imageUploader'> 
+                            <input type='file' className='fileInput' onChange={this.handleChange.bind(this)} />
+                            <button style = {style} onClick = {this.handleUplaod.bind(this)} className="btn btn-primary"><label>Upload</label></button>
+                        </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+function mapDispatchToProps(dispatch) {
+    return {
+        saveImage : (image) => {
+            //console.log("In mapDispatch" + imageDetails + "......" + imageName);
+            axios.post('http://localhost:3001/saveImage', image)
+            .then( (response) => {
+                
+            })
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Imageuploader);
