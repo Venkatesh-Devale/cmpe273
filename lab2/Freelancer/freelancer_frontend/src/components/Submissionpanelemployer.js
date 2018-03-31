501aa1f5882db416386800b356e2fbb40bde2d96
venkatesh-devale-sjsu <venkatesh.devale@sjsu.edu>
2018-03-31 16:57:52 -0700

    Addition of submission panel as separate components
    Addition of submission panel as separate components for freelancer and employer

---
+++ lab2/Freelancer/freelancer_frontend/src/components/Submissionpanelemployer.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Submissionpanelemployer extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            comment: ''
+        };
+
+    }
+
+
+    componentWillMount() {
+        var projectdetails = {
+            projectid: this.props.projectid
+        }
+        axios.post('http://localhost:3001/getworkercomment', projectdetails, {withCredentials: true})
+            .then((response) => {
+                console.log('Worker posted the comment', response.data);
+                if(response.data.comment !== null || response.data.comment !== '') {
+                    this.setState({
+                        comment: response.data
+                    })
+                }
+            })
+    }
+
+    render() {
+        return(
+            <div className='Submissionpanelemployer'>
+
+                <div id = 'divSubmissionPanel'>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="workerComments"><h4>Submission Panel - View Freelancer Comments</h4></label>
+                            <textarea className="form-control" value={this.state.comment} name='comment' disabled id="workerComments" col="5" rows="3" ></textarea>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Submissionpanelemployer;
---
+++ lab2/Freelancer/freelancer_frontend/src/components/Submissionpanelworker.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import '../css/style.css';
+import axios from 'axios';
+
+class Submissionpanelworker extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            comment:''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log("Project details", this.props.projectid, this.props.employer, this.props.worker, this.state.comment);
+        var projectComment = {
+            projectid: this.props.projectid,
+            comment: this.state.comment
+        }
+        axios.post('http://localhost:3001/insertworkercomment', projectComment, {withCredentials: true})
+            .then((response) => {
+                console.log(response.data);
+                if(response.data === 'Comment Updated Successfully') {
+                    alert('Comment posted Successfully');
+                    document.getElementById('workerComments').value = '';
+                }
+            })
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+    render() {
+
+        return(
+            <div className="Submissionpanelworker">
+                <div id = 'divSubmissionPanel'>
+                    <form onSubmit={this.handleSubmit}>
+
+                            <div className="form-group">
+                                <label htmlFor="workerComments"><h4>Submission Panel - Submit File and Comments</h4></label>
+                                <textarea className="form-control" name='comment' onChange={this.handleChange} id="workerComments" col="5" rows="3" placeholder='Please enter your comments here'></textarea>
+                            </div>
+                            <div className="input-group">
+                                <div className="custom-file">
+                                    <input type="file" className="custom-file-input" id="workerFileUpload"/>
+                                        <label className="custom-file-label" htmlFor="workerFileUpload">Choose file</label>
+                                </div>
+                                <div className="input-group-append">
+                                    <button className="btn btn-outline-secondary" type="submit">Upload</button>
+                                </div>
+                            </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Submissionpanelworker;
