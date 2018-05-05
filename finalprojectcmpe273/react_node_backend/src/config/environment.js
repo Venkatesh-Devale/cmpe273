036ad086769b6f74a4a2288fdb0bcab834db8674
venkatesh-devale-sjsu <venkatesh.devale@sjsu.edu>
2018-05-04 20:30:16 -0700

    Adding Project Fandango done for CMPE 273
    Adding Project Fandango done for CMPE 273

---
+++ finalprojectcmpe273/react_node_backend/src/config/environment.js
@@ -0,0 +1,18 @@
+
+
+//Local
+var envURL = 'http://13.57.176.22:3001/'
+var reactURL= 'http://13.57.176.22:3000/'
+
+
+//EC2 Instance
+//var ip = 'http://13.57.220.117:'
+//var reactURL= ip+'3000/'
+//var envURL = ip+'3001/'
+
+
+export{
+envURL,
+reactURL
+}
+
